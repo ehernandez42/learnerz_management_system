12cd02c31e1480b1d94b87a78faaa1c7b91e5e55
Eleazar <eleazarfhernandez@gmail.com>
2024-05-30 10:26:56 -0500

    Init

---
+++ backend/learnerz_backend/src/main/java/com/learnerz_backend/LearnerzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.learnerz_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerzBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerzBackendApplication.class, args);
+	}
+
+}
